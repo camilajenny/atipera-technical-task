9e2deff3c54b66f56827d75f9a5cdea77e739fb6
Kamila Łopuszanska <kamil.lopuszanski@gmail.com>
2025-08-16 17:47:24 +0200

    use JSONassert

--- src/test/java/com/atipera/demo/integration/GithubRepositoryIntegrationTest.java
+++ src/test/java/com/atipera/demo/integration/GithubRepositoryIntegrationTest.java
@@ -2,30 +2,27 @@ package com.atipera.demo.integration;
 
 import com.atipera.demo.controller.GithubRepositoryController;
 import com.atipera.demo.dto.RepositoryDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@ActiveProfiles("test")
 @EnableWireMock
-@TestPropertySource("classpath:application-test.yml")
+@ConfigureWireMock(filesUnderClasspath = "wiremock")
 class GithubRepositoryIntegrationTest {
 
     @Autowired
     private WebTestClient client;
-
-    @Value("${wiremock.server.baseUrl}")
-    private String wireMockUrl;
 
     @Test
     void givenValidUser_whenGetRepositories_thenCorrectNonForkReposWithBranches() throws Exception {
@@ -43,21 +40,26 @@ class GithubRepositoryIntegrationTest {
                 .returnResult()
                 .getResponseBody();
 
+        var mapper = new ObjectMapper();
+        var actualJson = mapper.writeValueAsString(repos);
+
         // then
-        assertNotNull(repos, "Response should not be null");
-
-        assertFalse(repos.isEmpty(), "Repos list should not be empty");
-        repos.forEach(repo -> {
-            assertNotNull(repo.repositoryName(), "Repository name should not be null");
-            assertNotNull(repo.ownerLogin(), "Owner login should not be null");
-
-            assertNotNull(repo.branches(), "Branches should not be null");
-            assertFalse(repo.branches().isEmpty(), "Branches list should not be empty");
-
-            repo.branches().forEach(branch -> {
-                assertNotNull(branch.name(), "Branch name should not be null");
-                assertNotNull(branch.lastCommitSha(), "Last commit SHA should not be null");
-            });
-        });
+        var expectedJson = """
+                [
+                    {
+                        "repositoryName": "git-consortium",
+                        "ownerLogin": "octocat",
+                        "branches": [
+                            {
+                              "name": "bower",
+                              "lastCommitSha": "f041b09c21e5b0e8e2308fce6c595c4df42f2633"
+                            }
+                        ]
+                    }
+                ]
+                """;
+        JSONAssert.assertEquals(expectedJson, actualJson, false);
     }
 }
+
+
